ee10a1ff9e6995b9c1c7aa1cd8797cca3f0cfeac
Nathan <--global>
2022-12-05 16:12:46 -0600

    Day 4 Completed
    Used ruby for day 4

---
+++ Day4/day4b.rb
@@ -0,0 +1,24 @@
+file = File.open("input.txt")
+t = 0
+
+def parse(str) 
+    arr = str.split(",")
+    a = arr[0]
+    b = arr[1]
+    aarr = a.split("-")
+    barr = b.split("-")
+    
+    arange = (Integer(aarr[0])..Integer(aarr[1])).to_a
+    brange = (Integer(barr[0])..Integer(barr[1])).to_a
+
+    crange = (arange & brange)
+    return crange.length > 0
+end
+
+file.each do |line|
+    if (parse(line)) 
+        t += 1
+    end
+    puts("Current count: #{t}")
+end
+
--- README.md
+++ README.md
@@ -4,3 +4,9 @@
  My attempts at solving the Advent of Code challenges for 2022.
  I intend to write my code in a different language each day as 
  an additional challenge.
+
+ ##Journal
+ - Day 1  - Java   - like riding a bike 
+ - Day 2  - Kotlin - like riding a bike but slightly different
+ - Day 3  - C++    - I forgot how fast C++ is
+ - Day 4  - Ruby   - If python sucked
