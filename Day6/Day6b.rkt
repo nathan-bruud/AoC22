797791b2a8b0831e0e6e7108e7df564571aa0eb9
nathan-bruud <97987326+nathan-bruud@users.noreply.github.com>
2022-12-06 15:28:13 -0600

    Update Day6b.rkt
    removed errant test case

--- Day6/Day6b.rkt
+++ Day6/Day6b.rkt
@@ -7,8 +7,6 @@
   (define chars (set->list (list->set (string->list str))))
   (= (string-length str) (length chars)))
 
-(all-chars-unique? "hello")
-
 (define checkFront (lambda (text bigIndex)
     (define subs (substring text 0 14))
     (if (all-chars-unique? subs) bigIndex (checkFront (substring text 1) (+ bigIndex 1)))
