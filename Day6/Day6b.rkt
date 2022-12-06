60b902af7643cfde9423b96f44424db3e15d1827
Nathan <93202154+nathan-pactum@users.noreply.github.com>
2022-12-06 16:09:35 -0600

    Update Day6b.rkt
    format

--- Day6/Day6b.rkt
+++ Day6/Day6b.rkt
@@ -9,7 +9,6 @@
 
 (define checkFront (lambda (text bigIndex)
     (define subs (substring text 0 14))
-    (if (all-chars-unique? subs) bigIndex (checkFront (substring text 1) (+ bigIndex 1)))
-)
-)
+    (if (all-chars-unique? subs) bigIndex (checkFront (substring text 1) (+ bigIndex 1)))))
+    
 (checkFront input 14)
